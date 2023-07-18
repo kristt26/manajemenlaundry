4df0b02e21b4a3b98338b3a38a1b9c4bcd51d11e
Ajenkris Yanto Kungkung <kristt26@gmail.com>
2023-07-18 18:51:30 +0900

    sd

--- app/Views/admin/kategori/index.php
+++ app/Views/admin/kategori/index.php
@@ -4,7 +4,11 @@
 <div class="col-md-12" ng-controller="kategoriController">
     <div class="row">
         <div class="col-md-4">
-
+            <div class="card">
+                <div class="card-body">
+                    
+                </div>
+            </div>
         </div>
         <div class="col-md-8">
             <div class="card">
